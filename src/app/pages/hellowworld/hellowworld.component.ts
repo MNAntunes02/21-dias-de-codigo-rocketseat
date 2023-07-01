18b9e70393b91c8c391849095b66e497e8b09ed4
Alisson <alissonam17@gmail.com>
2023-07-01 18:35:22 -0300

    Atualização de reponsividade

--- src/app/pages/hellowworld/hellowworld.component.ts
+++ src/app/pages/hellowworld/hellowworld.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
+import { gsap } from 'gsap';
+import { TextPlugin } from 'gsap/all';
+
+gsap.registerPlugin(TextPlugin);
 
 @Component({
   selector: 'app-hellowworld',
   templateUrl: './hellowworld.component.html',
   styleUrls: ['./hellowworld.component.css']
 })
-export class HellowworldComponent implements OnInit {
+export class HellowworldComponent implements OnInit , AfterViewInit{
 
-  constructor() { }
+  @ViewChild('hello') hello!: ElementRef<HTMLParagraphElement>;
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    // gsap.to(this.hello.nativeElement, {duration: 2, text: "This is the new text", ease: "none"});
   }
 
 }
